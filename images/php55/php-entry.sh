0b0b38ecd671eb7e81ce018843039171c0ba3b89
Roman NL <rnique@steperu.com>
2016-07-07 05:30:22 -0500

    Fixed path

--- images/php55/php-entry.sh
+++ images/php55/php-entry.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 cd /etc
 sed -i -e '/^error_log/s/^/error_log=${PHP_FPM_ERROR_LOG} ;/' \
         -e '/^php_value[session.save_handler]/s/^/php_value[session.save_handler] = ${PHP_SESSION_SAVE_HANDLER} ;/' \
-        -e '/^php_value[session.save_path]/s/^/php_value[session.save_path] = ${PHP_SESSION_SAVE_PATH} ;/' php-fpm.conf
+        -e '/^php_value[session.save_path]/s/^/php_value[session.save_path] = ${PHP_SESSION_SAVE_PATH} ;/' php-fpm.d/docker.conf
 
 exec php-fpm
